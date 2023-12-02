ba4b46d0a0152bfba405046db8ad635cb5a7fbd0
zmomen <zalmomen01@gmail.com>
2023-12-02 12:24:45 -0600

    day 1 solution

--- java_2023/src/Day1.java
+++ java_2023/src/Day1.java
@@ -1,6 +1,43 @@
-public class Day1 {
-    public static void main(String[] args) {
-        System.out.println("Hello day1");
+import utils.Day;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day1 extends Day {
+    public void solve(int part) {
+        String content = this.readFile("in_d1p1.txt");
+
+        List<Integer> nums = new ArrayList<>();
+        var lines = content.split("\n");
+        for (String line : lines) {
+            if (part == 2) {
+                line = line.replaceAll("one", "o1e").replaceAll("two", "t2o").replaceAll("three", "t3e")
+                        .replaceAll("four", "f4r").replaceAll("five", "f5e").replaceAll("six", "s6x")
+                        .replaceAll("seven", "s7n").replaceAll("eight", "e8t").replaceAll("nine", "n9e");
+            }
+            var charArray = line.toCharArray();
+            char first = '0';
+            char last = '0';
+
+            for (int i = 0; i < charArray.length; i++) {
+                if (Character.isDigit(line.charAt(i))) {
+                    first = line.charAt(i);
+                    break;
+                }
+            }
+
+            for (int j = charArray.length - 1; j >= 0; j--) {
+                if (Character.isDigit(line.charAt(j))) {
+                    last = line.charAt(j);
+                    break;
+                }
+            }
+
+            var combinedNumber = Integer.parseInt(Character.toString(first) + Character.toString(last));
+            nums.add(combinedNumber);
+        }
+        var result = nums.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(result);
     }
 
 }
---
+++ java_2023/src/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+
+    public static void main(String[] args) {
+        Day1 d1 = new Day1();
+        d1.solve(1);
+        d1.solve(2);
+    }
+}
